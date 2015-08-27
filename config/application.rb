748fb9f311d80164ebd1cc77a20f326dc0db8469
William Bolton <william.ellet@gmail.com>
2015-08-27 10:29:22 -0400

    debugging heroku commit

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module ParkingDataApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    configure(:development){set :database, "sqlite3:blog.sqlite3"}
+    # configure(:development){set :database, "sqlite3:blog.sqlite3"}
     
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
